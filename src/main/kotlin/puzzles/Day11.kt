f3a8347ea516298042f20fffaf9e5b4686d503c5
Vladimir Burmistrovich <vladibur@amazon.com>
2023-12-13 16:18:07 -0500

    day11 part2

--- src/main/kotlin/puzzles/Day11.kt
+++ src/main/kotlin/puzzles/Day11.kt
@@ -1,44 +1,57 @@
 package puzzles
 
 class Day11 : Puzzle(11) {
+    val galaxyPairs = findGalaxyPairs()
     override fun partOne() {
-        val galaxyPairs = findGalaxyPairs()
-        val sum =
-            galaxyPairs.sumOf {
-                Math.abs(it.first.x - it.second.x).toLong() + Math.abs(it.first.y - it.second.y)
-                    .toLong()
-            }
+        val sum = getSum(2)
         println("Part 1: $sum")
     }
 
     override fun partTwo() {
-        TODO("Not yet implemented")
+        val sum = getSum(1000000)
+        println("Part 2: $sum")
     }
 
-    fun expandUniverse(): List<List<Char>> {
-        val result = mutableListOf<MutableList<Char>>()
-        for (i in input.indices) {
-            result.add(input[i].toMutableList())
-            if (input[i].all { it == '.' }) {
-                result.add(input[i].toMutableList())
+    private fun getSum(expandTo: Int): Long {
+        val expandedX = expandX(expandTo)
+        val expandedY = expandY(expandTo)
+        val sum =
+            galaxyPairs.sumOf {
+                var path = 0L
+                val minY = it.first.y.coerceAtMost(it.second.y)
+                val maxY = it.first.y.coerceAtLeast(it.second.y)
+                for (y in minY..<maxY) {
+                    path += expandedY[y]
+                }
+                val minX = it.first.x.coerceAtMost(it.second.x)
+                val maxX = it.first.x.coerceAtLeast(it.second.x)
+                for (x in minX..<maxX) {
+                    path += expandedX[x]
+                }
+                path
             }
+        return sum
+    }
+
+    fun expandY(to: Int): List<Int> {
+        return input.map { y ->
+            if (y.all { it == '.' }) to
+            else 1
         }
-        var added = 0
-        for (i in input[0].indices) {
-            if (input.all { it[i] == '.' }) {
-                result.map { it.add(i + added, '.') }
-                added++
-            }
+    }
+
+    fun expandX(to: Int): List<Int> {
+        return input[0].mapIndexed { x, _ ->
+            if (input.all { it[x] == '.' }) to
+            else 1
         }
-        return result
     }
 
     fun findGalaxies(): List<Point> {
-        val expanded = expandUniverse()
         val result = mutableListOf<Point>()
-        for (i in expanded.indices) {
-            for (j in expanded[i].indices) {
-                if (expanded[i][j] == '#') {
+        for (i in input.indices) {
+            for (j in input[i].indices) {
+                if (input[i][j] == '#') {
                     result.add(Point(i, j))
                 }
             }
